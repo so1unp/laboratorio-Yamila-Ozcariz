5b2c548f14c464e0fbd4e7ef04b7ee799352ea21
yamila <yami.ozcariz@hotmail.com>
2024-03-14 18:50:42 -0300

    ejercicio 2

--- lab-01/sum.c
+++ lab-01/sum.c
@@ -5,6 +5,19 @@ int main(int argc, char *argv[])
 {
     // Agregar código aquí.
 
+	int i;
+	int s = 0; // suma de los valores dados
+
+	for(i = 0 ; i < argc ; i = i + 1) {
+	
+		s = s + atoi(argv[i]);
+	
+	}
+
+	printf("El resultado es: %d\n",s);
+
+
+
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
