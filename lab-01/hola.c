e5c8555189dd855cc34b44816070f66669fee975
yamila <yami.ozcariz@hotmail.com>
2024-03-14 18:49:33 -0300

    ejercicio 1

--- lab-01/hola.c
+++ lab-01/hola.c
@@ -4,7 +4,22 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+	// printf("hola mundo\n");
+	
+	int i;
 
+	for ( i = 1 ; i < argc  ; i = i + 1 ){
+		
+		if (i < argc - 1) {
+		
+			printf("%s ",argv[i]);
+		
+		} else {
+			printf("%s\n",argv[i]);
+		}
+	
+	}
+	
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
