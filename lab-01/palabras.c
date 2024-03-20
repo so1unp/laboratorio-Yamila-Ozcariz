04a18123efded8868c56866cd95e41edcd2720f5
yamila <yami.ozcariz@hotmail.com>
2024-03-20 20:32:03 -0300

    ejercicio 3

--- lab-01/palabras.c
+++ lab-01/palabras.c
@@ -4,7 +4,22 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+	int c;
 
+	while (c != EOF ){
+	
+		c = getchar();
+
+		if (c == ' ') { // si "espacio" entonces pasar a sig linea
+			printf("%c\n",' ');
+
+		} else {
+		
+			printf("%c",c);
+		}
+		
+	} 
+	// REVISAR!!!!!
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
 }
